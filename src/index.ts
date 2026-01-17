547060833a4b906fc9af3762754627f8091abb41
protechassist36-admin <protechassist36@gmail.com>
2026-01-17 13:47:59 +0000

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, Welcome to Ptal API!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
